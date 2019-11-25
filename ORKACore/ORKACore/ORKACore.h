c6a629dc9fde1be762c3a880dac39ab41f59ad2d
Amit Majumdar <amit.majumdar89@gmail.com>
2019-11-25 09:22:26 +0530

    Initial Commit

---
+++ ORKACore/ORKACore/ORKACore.h
@@ -0,0 +1,19 @@
+//
+//  ORKACore.h
+//  ORKACore
+//
+//  Created by Amit Majumdar on 25/11/19.
+//  Copyright © 2019 ORKA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ORKACore.
+FOUNDATION_EXPORT double ORKACoreVersionNumber;
+
+//! Project version string for ORKACore.
+FOUNDATION_EXPORT const unsigned char ORKACoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ORKACore/PublicHeader.h>
+
+
---
+++ ORKACore/ORKACoreExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ORKACoreExample
+//
+//  Created by Amit Majumdar on 25/11/19.
+//  Copyright © 2019 ORKA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
